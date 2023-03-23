e3400a57ebdda82d0d698daf2e982453a8529a50
Aaron Hooper <rnjckhpr@gmail.com>
2023-03-23 18:41:48 +0000

    Add TypeScriptLinter to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import VitePluginLinter from 'vite-plugin-linter'
+
+const { linterPlugin, TypeScriptLinter } = VitePluginLinter
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    linterPlugin({
+      include: ["./src/**/*.ts", "./src/**/*.tsx"],
+      linters: [new TypeScriptLinter()],
+    })
+  ],
 })
