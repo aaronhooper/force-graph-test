953f4954be98102d2e7e279bf81a9323354f5a6e
Aaron Hooper <rnjckhpr@gmail.com>
2023-03-23 17:45:59 +0000

    Embed component in App and extract genRandomTree into util

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { ForceGraph2D } from 'react-force-graph'
-
-function genRandomTree(N = 300, reverse = false) {
-  return {
-    nodes: [...Array(N).keys()].map(i => ({ id: i })),
-      links: [...Array(N).keys()]
-    .filter(id => id)
-    .map(id => ({
-      [reverse ? 'target' : 'source']: id,
-      [reverse ? 'source' : 'target']: Math.round(Math.random() * (id-1))
-    }))
-  };
-}
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ForceGraph2D graphData={genRandomTree()} />
+    <App />
   </React.StrictMode>,
 )
