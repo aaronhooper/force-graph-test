1eab4e36bb437d875c1c97216dfbeb8ca6e6c903
Aaron Hooper <rnjckhpr@gmail.com>
2023-03-23 17:08:06 +0000

    Add component to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import { ForceGraph2D } from 'react-force-graph'
+
+function genRandomTree(N = 300, reverse = false) {
+  return {
+    nodes: [...Array(N).keys()].map(i => ({ id: i })),
+      links: [...Array(N).keys()]
+    .filter(id => id)
+    .map(id => ({
+      [reverse ? 'target' : 'source']: id,
+      [reverse ? 'source' : 'target']: Math.round(Math.random() * (id-1))
+    }))
+  };
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ForceGraph2D graphData={genRandomTree()} />
   </React.StrictMode>,
 )
