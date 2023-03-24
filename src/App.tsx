bbacbccd86341f8cb1bfacb0f46d70a72d220f94
Aaron Hooper <rnjckhpr@gmail.com>
2023-03-24 12:05:09 +0000

    Add nodes to graph on interval iteration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import "./App.css";
-import { genRandomTree } from "./util";
 import { ForceGraph2D } from "react-force-graph";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [graphData, setGraphData] = useState({
+    nodes: [{ id: 0 }],
+    links: [],
+  });
+
+  console.log(graphData);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setGraphData(({ nodes, links }) => {
+        const id = nodes.length;
+        return {
+          nodes: [...nodes, { id }],
+          links: [
+            ...links,
+            { source: id, target: Math.round(Math.random() * (id - 1)) },
+          ],
+        };
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <ForceGraph2D graphData={genRandomTree()} />
+      <ForceGraph2D graphData={graphData} />
     </div>
   );
 }
