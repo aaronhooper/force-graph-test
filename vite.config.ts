f99f832001c25a2354eca73edb893a4af1ba1073
Aaron Hooper <rnjckhpr@gmail.com>
2023-03-24 11:10:00 +0000

    Remove linter plugin from config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import VitePluginLinter from "vite-plugin-linter";
-
-const { linterPlugin, TypeScriptLinter } = VitePluginLinter;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    linterPlugin({
-      include: ["./src/**/*.ts", "./src/**/*.tsx"],
-      linters: [new TypeScriptLinter()],
-    }),
-  ],
+  plugins: [react()],
 });
