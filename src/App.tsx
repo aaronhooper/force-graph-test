45a2249cb38c270f2970f31d8e0146e2f077fc44
Aaron Hooper <rnjckhpr@gmail.com>
2023-03-25 11:13:26 +0000

    Add getArtists function

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,20 @@ import "./App.css";
 import { ForceGraph2D } from "react-force-graph";
 import { useCallback, useEffect, useState } from "react";
 
+async function getArtists(query, limit) {
+  const apiKey = "269da84a1701721e5910142a21121af4";
+  const url = `//ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${query}&api_key=${apiKey}&limit=${limit}&format=json`;
+
+  const response = await fetch(url);
+  const json = await response.json();
+
+  if (json.hasOwnProperty("error")) {
+    throw new Error(json.message);
+  }
+
+  return json.similarartists.artist.map(({ name, url }) => ({ name, url }));
+}
+
 function App() {
   const [graphData, setGraphData] = useState({
     nodes: [{ id: 0 }],
